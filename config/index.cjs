792c40ca61bf1f89052e3497f2681d1d430851ed
henry666666 <2209741665@qq.com>
2022-10-24 21:36:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,43 +57,7 @@ const USER_CONFIG = {
     courseSchedule: true,
   },
 
-
-  USERS: [
-    {
-	  openurl: "https://music.163.com/song?id=473285827&userid=430633625",
-      // 想要发送的人的名字
-      name: '文静小宝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oR9X45zu65-bw7Vq41ME0qOvTBjg',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-20',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: 'ALIEN', year: '2001', date: '04-03',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '文静', year: '2000', date: '01-20',
-        },
-        {
-          type: '节日', name: '表白纪念日', year: '2022', date: '04-22',
-        },
-        {
-          type: '节日', name: '新年春节', year: '2022', date: '01-22',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-04-22' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-01-22' },
-      ],
-    },
-      courseSchedule: {
+  courseSchedule: {
         // 单双周的基准
         benchmark: {
           // 这里设置一个日期，用来作为判断课表是否单双周的依据
@@ -149,11 +113,46 @@ const USER_CONFIG = {
         }
       },
     },
+
+  USERS: [
+    {
+	  openurl: "https://music.163.com/song?id=473285827&userid=430633625",
+      // 想要发送的人的名字
+      name: '文静小宝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oR9X45zu65-bw7Vq41ME0qOvTBjg',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '1',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '01-20',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: 'ALIEN', year: '2001', date: '04-03',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '文静', year: '2000', date: '01-20',
+        },
+        {
+          type: '节日', name: '表白纪念日', year: '2022', date: '04-22',
+        },
+        {
+          type: '节日', name: '新年春节', year: '2022', date: '01-22',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-04-22' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-01-22' },
+      ],
+    },
   ],
 
-
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '11',
+  CALLBACK_TEMPLATE_ID: 'IovLEZIijT8PqbDT2J0KOCwSuI-YmGaCCiD0aU4a23c',
 
   CALLBACK_USERS: [
     {
