60e42aebaa423e92e9300873e0fc2600f94bc540
henry666666 <2209741665@qq.com>
2022-10-24 21:49:51 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
-    id: '0001',
+    id: 'fdF8XaPAE6AgJ25o2cNMZ-75fz7m1d-CbVVUFTpQ7JY',
     title: '亲爱的, 早上好',
     desc: `
       **{{date.DATA}}**
@@ -36,7 +36,7 @@ const TEMPLATE_CONFIG = [
     `
   },
   {
-    id: '0002',
+    id: 'IovLEZIijT8PqbDT2J0KOCwSuI-YmGaCCiD0aU4a23c',
     title: '推送完成提醒',
     desc: `
       服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
